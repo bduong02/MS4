36be90fae26bbdd95ae8a8c9409e1b1d170e72e0
Ishan <ishanparikh18@gmail.com>
2023-04-01 19:57:39 -0700

    Milestone 1: DBEnvironment init added, skeleton for Parsing to be completed

--- Parser.cpp
+++ Parser.cpp
@@ -1,15 +1,59 @@
-//Filename: Parser.cpp
+//Filename: driver.cpp
 //Authors: Ishan Parikh, Bryan
 //Date 4-1-23
 //CPSC 5300
 //Purpose: SQL statement parser impelementation in C++
 //--not extensive, test-driven only to sample input at the moment
 #include <iostream>
+#include <cstdlib> 
 #include <string>
 #include "db_cxx.h"
 #include "SQLParser.h"
+#include "SQLParserResult.h"
+using namespace std;
+
+//Preconditions: the SqlParseResult parse tree MUST be processed 
+//               beforehand by the SQLParser::parseSQLString function
+//Process: for now, just returns the resulting statement result as a string
+//Postconditions: none
+string execute(hsql::SQLParserResult* result);
 
 int main(int argc, char* argv[]) {
-    std::cout << "Test, test" << std::endl;
+    //handle cmd arguments (will ignore extra args)
+    if(argc <= 1) {
+        return 1;
+    }
+    string envDirPath = argv[1];
+
+    //initialize DB environment in passed directory path
+    DbEnv environment(0U);
+    try {
+        environment.set_message_stream(&cout);
+	    environment.set_error_stream(&cerr);
+	    environment.open(envDirPath.c_str(), DB_CREATE, 0);
+    } catch(DbException &E) {
+        cout << "Error creating DB environment" << endl;
+        return 1;
+    }
+	
+
+    //read sql statements, parse & handle accordingly
+    const string EXIT_RESPONSE = "quit";
+    string statementInput = "";
+    while(statementInput != EXIT_RESPONSE) {
+        
+        cout << "SQL> ";
+        cin >> statementInput;
+        hsql::SQLParserResult* result = nullptr;
+        if(statementInput != EXIT_RESPONSE) {
+            //include logic here to handle
+        }
+    } 
     return 0;
+}
+
+
+string execute(hsql::SQLParserResult* result) {
+    //handle logic etc...
+    return "";
 }
