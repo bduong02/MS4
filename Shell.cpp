bdb8beca22097e80bd703e1830a26eddf70a0a52
Ishan <ishanparikh18@gmail.com>
2023-04-17 23:18:24 -0700

    Extended milestone a bit

--- Shell.cpp
+++ Shell.cpp
@@ -17,9 +17,12 @@ DbEnv *_DB_ENV;
 
 //Preconditions: the SqlParseResult parse tree MUST be processed 
 //               beforehand by the SQLParser::parseSQLString function
-//Process: for now, just returns the resulting statement result as a string
+//Process:       just matches the result to a string and prints it
 //Postconditions: none
-string execute(SQLParserResult* &result, string statementInput);
+void execute(SQLParserResult* &result);
+string handleSelect(const SelectStatement *stmt);
+string handleCreate(const CreateStatement *stmt);
+string expressToString(const Expr* expression);
 
 int main(int argc, char* argv[]) {
     if(argc <= 1) {
@@ -48,10 +51,10 @@ int main(int argc, char* argv[]) {
         cout << "Slotted Page Tests passed!" << endl;
     }
 
-    if(MS2Tests::slottedPageTests() == false) {
-        cout << "Slotted Page Tests failed! Moving on to shell...." << endl << endl;
+    if(MS2Tests::heapFileAndTableTests() == false) {
+        cout << "Heap Tests failed! Moving on to shell...." << endl << endl;
     } else {
-        cout << "Slotted Page Tests passed! Moving on to shell...." << endl << endl;
+        cout << "Heap Tests passed! Moving on to shell...." << endl << endl;
     }
 
     //read, parse, & handle sql statements 
@@ -62,37 +65,191 @@ int main(int argc, char* argv[]) {
         getline(cin, statementInput);
 
         SQLParserResult* result = nullptr;
+
         //handle input with appropriate action or quit
         if(statementInput != EXIT_RESPONSE) {
 	        result = SQLParser::parseSQLString(statementInput);
-		    cout << execute(result, statementInput) << endl;
-            delete result;
-	    } else {
-	        exit(EXIT_FAILURE);
-	    } 
+		    
+            if(!result->isValid()) {
+                delete result;
+                cout << "Invalid SQL: " <<  statementInput << endl;
+            } else {
+                execute(result);
+                delete result;
+            }     
+	    }
     } 
 
     return 0;
 }
-string execute(SQLParserResult* &result, string statementInput) {
-    string resultStr = "";
-    //handle invalid statement
-    if(!result->isValid()) {
-        return "Invalid SQL: " + statementInput;
-    } 
 
+
+void execute(SQLParserResult* &result) {
     //handle sql statements accordingly
-    SQLStatement* inputStatement = result->getMutableStatement(0);
-    switch(inputStatement->type()) {
-        case kStmtSelect:
-            resultStr = "";
-            break;
-        case kStmtCreate:
-            break;
-        default:
-            resultStr = "Invalid SQL: " + statementInput;
-            break;
+    for(uint i = 0; i < result->size(); ++i) {
+        switch(result->getStatement(i)->type()) {
+            case kStmtSelect:
+                cout << handleSelect((const SelectStatement *) result->getStatement(i)) << endl;
+                break;
+            case kStmtCreate:
+                cout << handleCreate((const CreateStatement *) result->getStatement(i)) << endl;
+                break;
+            default:
+                cout << "Invalid SQL/Not supported: " <<  endl;
+                break;
+        }
     }
-    return resultStr;   
 }
 
+//note this is not super extensive
+string handleSelect(const SelectStatement *stmt) {
+    string output = "SELECT ";
+    bool hasComma = false;
+
+    //handle each expression
+    for (Expr *expr : *stmt->selectList) {
+        if (hasComma)
+            output += ", ";
+
+        //handle typess
+        switch (expr->type) {
+            case kExprOperator:
+                if(expr == nullptr)
+                    output += "null";
+                else {
+                    //if unary not
+                    if (expr->opType == Expr::NOT)
+                        output += "NOT ";
+
+                    // Left expression
+                    output += expressToString(expr->expr) + " ";
+
+                    //handle operator converted
+                    switch (expr->opType) {
+                        case Expr::OR:
+                            output += "OR";
+                            break;
+                        case Expr::SIMPLE_OP:
+                            output += expr->opChar;
+                            break;
+                        case Expr::AND:
+                            output += "AND";
+                            break;
+                        default:
+                            break;
+                    }
+
+                    // Right expression converted
+                    if (expr->expr2 != nullptr)
+                        output += " " + expressToString(expr->expr2);
+                }
+                break;
+            case kExprLiteralFloat:
+                output += to_string(expr->fval);
+                break;
+            case kExprLiteralInt:
+                output += to_string(expr->ival);
+                break;
+            case kExprColumnRef:
+                if (expr->table != nullptr)
+                    output += string(expr->table) + ".";
+            case kExprLiteralString:
+                output += expr->name;
+                break;
+            case kExprStar:
+                output += "*";
+                break;
+
+            default:
+                output += "(UNDEFINED)";  //undefined expression tyep
+                break;
+        }
+
+        if (expr->alias != nullptr) output += string(" AS ") + string(expr->alias);
+
+
+        hasComma = true;
+    }
+    output += " FROM ";
+
+    if (stmt->whereClause != nullptr)
+        output += " WHERE " + expressToString(stmt->whereClause);
+
+    return output;
+}
+
+string handleCreate(const CreateStatement* stmt) {
+    string output = "CREATE TABLE ";
+    if (stmt->type != CreateStatement::kTable)
+        return output + "...";
+    if (stmt->ifNotExists)
+        output += "IF NOT EXISTS ";
+    
+    //handle table name 
+    output += string(stmt->tableName) + " (";
+    bool hasComma = false;
+
+    //handle column types for table
+    for (ColumnDefinition *col : *stmt->columns) {
+        if (hasComma)
+            output += ", ";
+
+        //account for different types
+        switch (col->type) {
+            case ColumnDefinition::DOUBLE:
+                output += " DOUBLE";
+                break;
+            case ColumnDefinition::INT:
+                output += " INT";
+                break;
+            case ColumnDefinition::TEXT:
+                output += " TEXT";
+                break;
+            default:
+                output += " ...";
+                break;
+        }
+        hasComma = true;
+    }
+
+    //close output and return
+    output += ")";
+    return output;
+}
+
+
+//note this is not super extensive
+string expressToString(const Expr* expression) {
+    string output = "";
+
+    //handle different expression types (handles most cases)
+    switch(expression->type) {
+        //handle special symbols
+        case kExprStar:
+            output += "*";
+            break;
+        case kExprColumnRef:
+            if (expression->table != NULL)
+                output += string(expression->table) + ".";
+        case kExprLiteralString:
+            output += expression->name;
+            break;
+
+        //convert the float and long values to a string
+        case kExprLiteralFloat:
+            output += to_string(expression->fval);
+            break;
+        case kExprLiteralInt:
+            output += to_string(expression->ival);
+            break;
+        
+        //undefined
+        default:
+            output += "(UNDEFINED)";
+            break;
+    }
+
+    if (expression->alias != nullptr) output+= string(" AS ") + expression->alias;
+
+    return output;
+}
