f49dfc696bb3a98494643e890885b4cf2add77ad
Sothaninn Sieng <94101972+Sothaninn@users.noreply.github.com>
2023-05-22 12:43:39 -0700

    Added the insert, del, select function header in SQLExec.h

--- SQLExec.h
+++ SQLExec.h
@@ -83,6 +83,12 @@ protected:
     static QueryResult *drop_index(const hsql::DropStatement *statement);
 
     static QueryResult *show_index(const hsql::ShowStatement *statement);
+    
+    static QueryResult *insert(const hsql::InsertStatement *statement);
+
+    static QueryResult *del(const hsql::DeleteStatement *statement);
+
+    static QueryResult *select(const hsql::SelectStatement *statement);
 
     /**
      * Pull out column name and attributes from AST's column definition clause
