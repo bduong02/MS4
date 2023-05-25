40107a3f69524a18644b50fd189158c92588dc0b
huyeunten <uyeuntenhale@seattleu.edu>
2023-05-25 09:04:53 -0700

    add new evalplan

--- EvalPlan.h
+++ EvalPlan.h
@@ -1,43 +1,26 @@
-/**
- * @file EvalPlan.h - Evaluation plans
- *
- * @author Kevin Lundeen
- * @see "Seattle University, CPSC5300, Winter 2023"
- */
 #pragma once
 
 #include "storage_engine.h"
 
-
 typedef std::pair<DbRelation *, Handles *> EvalPipeline;
 
 class EvalPlan {
-public:
-    enum PlanType {
-        ProjectAll, Project, Select, TableScan
-    };
+    public:
+        enum PlanType {
+            Project, Select, TableScan
+        };
+    
+    EvalPlan(EvalPlan *relation); // select
+    EvalPlan(ColumnNames *projection, EvalPlan *relation); // project
+    EvalPlan(DbRelation &table); // TableScan
 
-    EvalPlan(PlanType type, EvalPlan *relation);  // use for ProjectAll, e.g., EvalPlan(EvalPlan::ProjectAll, table);
-    EvalPlan(ColumnNames *projection, EvalPlan *relation); // use for Project
-    EvalPlan(ValueDict *conjunction, EvalPlan *relation);  // use for Select
-    EvalPlan(DbRelation &table);  // use for TableScan
-    EvalPlan(const EvalPlan *other);  // use for copying
     virtual ~EvalPlan();
-
-    // Attempt to get the best equivalent evaluation plan
-    EvalPlan *optimize();
-
-    // Evaluate the plan: evaluate gets values, pipeline gets handles
     ValueDicts *evaluate();
-
     EvalPipeline pipeline();
 
-protected:
-
-    PlanType type;
-    EvalPlan *relation;  // for everything except TableScan
-    ColumnNames *projection;  // for Project
-    ValueDict *select_conjunction;  // for Select
-    DbRelation &table;  // for TableScan
+    protected:
+        PlanType type;
+        EvalPlan *relation;
+        ColumnNames *projection;
+        DbRelation &table;
 };
-
